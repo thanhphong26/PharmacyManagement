c9c2db1cca5b8c563049144066b4bfefda32e653
Nguyễn Quang Vinh VT <nguyenquangvinhwork1207@gmail.com>
2023-10-21 20:36:19 +0700

    chỉnh size form main

--- PharmacyManagement/Form1.cs
+++ PharmacyManagement/Form1.cs
@@ -16,5 +16,10 @@ namespace PharmacyManagement
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
