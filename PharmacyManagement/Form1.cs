b9bfd9a5cc30a98ee4d0968278028d2bd606375f
NguyênNguyên <billbatri085@gmail.com>
2023-10-25 11:40:43 +0700

    Chỉnh tên đối tượng của FLogin

--- PharmacyManagement/Form1.cs
+++ PharmacyManagement/Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -16,8 +16,8 @@ namespace PharmacyManagement
         public Form1()
         {
             InitializeComponent();
-            FLogin fMain = new FLogin();
-            Application.Run(fMain);
+            FLogin flogin = new FLogin();
+            Application.Run(flogin);
         }
 
     }
