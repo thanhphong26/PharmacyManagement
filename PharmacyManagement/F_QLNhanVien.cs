0bbde2038672e461860f83a07b5c6190c3a4ca30
Thanh Phong <thanh2652003abc@gmail.com>
2023-10-26 09:28:37 +0700

    Add F_QLNhanVien

---
+++ PharmacyManagement/F_QLNhanVien.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PharmacyManagement
+{
+    public partial class F_QLNhanVien : Form
+    {
+        public F_QLNhanVien()
+        {
+            InitializeComponent();
+        }
+        private Panel panelFromForm1;
+
+        public F_QLNhanVien(Panel panel)
+        {
+            InitializeComponent();
+            panelFromForm1 = panel;
+        }
+    }
+}
